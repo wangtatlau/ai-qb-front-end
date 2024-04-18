76d6abc1281af2e9bedc797718b1cc474662e2ac
Wang Tat Lau (k22023660) <wang.lau@kcl.ac.uk>
2024-04-19 00:26:16 +0100

    .

--- src/components/ui/EmptyPage.js
+++ src/components/ui/EmptyPage.js
@@ -4,7 +4,7 @@ import MainSidebarLayout from "../layout/MainSidebarLayout";
 const EmptyPage = () => {
   return (
     <MainSidebarLayout>
-      <h1 style={{marginLeft: "400px"}}>Currently working in it...</h1>
+      <h1 style={{marginLeft: "400px"}}>Currently working on it...</h1>
     </MainSidebarLayout>
   );
 };
